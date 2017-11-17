a07f4c96966c6c3924505166ff65216fcb296235
Cristina Negrean <cnegrean@xebialabs.com>
2017-11-17 16:52:07 +0100

    exclude from test coverage report methods annotated by lombok/Generated and disable async processing

--- src/main/java/cristina/tech/blog/travel/WanderlustApplication.java
+++ src/main/java/cristina/tech/blog/travel/WanderlustApplication.java
@@ -5,14 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 
 @SpringBootApplication
 @EntityScan(
         basePackageClasses = { WanderlustApplication.class, Jsr310JpaConverters.class }
 )
-@EnableAsync //enabling asynchronous processing
 public class WanderlustApplication {
 
     public static void main(String[] args) {
